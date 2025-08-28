ddfcbb5d1647f56ee902d4eb85d0d5f82b4c4925
nurullah305 <snurullahstudentboy@gmail.com>
2025-08-28 19:46:04 +0900

    added call aleart

--- script.js
+++ script.js
@@ -1,7 +1,117 @@
-document.getElementById("heart").addEventListener("click",  function increaseHeart() {
-  
-     const heartNumber = parseInt(document.getElementById("heart-number").innerText)
-     
-    
-   console.log(heartNumber)
- } )
+document.getElementById("nationalEmergency").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling National Emergency Number 999...")
+
+
+})
+
+
+document.getElementById("police").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling Police Helpline Number 999...")
+
+
+})
+
+
+document.getElementById("fire").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling Fire Service Number 999...")
+
+
+})
+
+document.getElementById("health").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling Ambulance Service 1994-999999...")
+
+
+})
+
+
+document.getElementById("women").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling Women & Child Helpline Number 109...")
+
+
+})
+
+document.getElementById("ach").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling Anti-Corruption Helpline Number 106...")
+
+
+})
+
+document.getElementById("electricity").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling  Electricity Helpline Number 16216...")
+
+
+})
+
+document.getElementById("brac").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling Brac Helpline Number 16445...")
+
+
+})
+
+document.getElementById("railway").addEventListener("click", function(e){
+  e.preventDefault
+ 
+      
+alert("Calling  Bangladesh Railway Helpline Number 163...")
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+  //const availableCoins = parseInt(document.getElementById("all-coin").innerText)
+
+
+
+ //totalavailableCoin = availableCoins - 20
+
+  //document.getElementById("availableCoins").innerText = totalavailableCoin
+
+// function setupHeartCounters() {
+//  const heartButtons = document.querySelectorAll("heart-btn");
+//
+//  heartButtons.forEach(button => {
+//    const countSpan = button.nextElementSibling;
+//
+//    button.addEventListener("click", () => {
+//      let count = parseInt(countSpan.textContent, 10);
+//      countSpan.textContent = count + 1;
+//    });
+//  });
+//}
+
+// Initialize when DOM is fully loaded
+//document.addEventListener("DOMContentLoaded", setupHeartCounters);
