82e64267fccfca75ceacf15bce1c77ed55d215af
nurullah305 <snurullahstudentboy@gmail.com>
2025-08-28 16:19:57 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("heart").addEventListener("click",  function increaseHeart() {
+  
+     const heartNumber = parseInt(document.getElementById("heart-number").value)
+    
+   console.log(heartNumber)
+ } )
